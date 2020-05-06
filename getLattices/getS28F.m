9a330116ce4a317d33da026289ef03b25c96757a
Nicola Carmignani <carmignani@MacCarmignani.local>
2020-05-06 10:53:16 +0200

    modified function to get S28F lattice

--- getLattices/getS28F.m
+++ getLattices/getS28F.m
@@ -3,6 +3,6 @@ function [ LOW_EMIT_RING_INJ,LOW_EMIT_RING,ARCA,ARCA_INJ,ARCB_INJ ] = ...
 %   [ LOW_EMIT_RING_INJ,LOW_EMIT_RING,ARCA,ARCA_INJ,ARCB_INJ ] = getS28F()
 % 
 
-load('/mntdirect/_machfs/carmigna/Repositories/EBSlattices/AT/S28F_PA.mat')
+load('S28F_PA.mat')
 
 end
