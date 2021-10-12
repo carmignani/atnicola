d9bdb931ac817121d3829af3d8215b2d4300946a
Nicola Carmignani <carmigna@raki3.esrf.fr>
2021-10-12 16:22:40 +0200

    getS28F updated

--- getLattices/getS28F.m
+++ getLattices/getS28F.m
@@ -3,6 +3,6 @@ function [ LOW_EMIT_RING_INJ,LOW_EMIT_RING,ARCA,ARCA_INJ,ARCB_INJ ] = ...
 %   [ LOW_EMIT_RING_INJ,LOW_EMIT_RING,ARCA,ARCA_INJ,ARCB_INJ ] = getS28F()
 % 
 
-load('S28F_PA.mat')
+load('/machfs/carmigna/Repositories/atnicola/getLattices/S28F_PA.mat')
 
 end
