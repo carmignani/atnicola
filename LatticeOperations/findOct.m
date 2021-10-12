e2c6764bd93ed4ec7c219e98cb52d0fea5bed8d5
Nicola Carmignani <carmigna@raki3.esrf.fr>
2021-10-12 16:20:59 +0200

    corrections to findoct and findsext

--- LatticeOperations/findOct.m
+++ LatticeOperations/findOct.m
@@ -1,9 +1,12 @@
 function [indoct,b4,L] = findOct(ring)
 %   [indsext,b4,L] = findOct(ring)
-%   this function returns the indexes of the sextupoles
-%   elements with PolynomB(1,4) not zero.
+%   this function returns the indexes, the strengths and the lengths of 
+%   the octupoles
+%
+%   see also findSext
 
-indoct=findcells(ring,'FamName','O[IJFD]\w*');
+indoct=findcells(ring,'FamName','O[IJFD]\w*')';
 b4=getcellstruct(ring,'PolynomB',indoct,1,4);
 L=getcellstruct(ring,'Length',indoct);
+
 end
--- LatticeOperations/findSext.m
+++ LatticeOperations/findSext.m
@@ -1,8 +1,12 @@
 function [indsext,b3,L] = findSext(ring)
 %   [indsext,b3,L] = findSext(ring)
-%   this function returns the indexes of the sextupoles
-%   elements with PolynomB(1,3) not zero.
-indsext=findcells(ring,'Class','Sextupole');
+%   this function returns the indexes, the strengths and the lengths of 
+%   the sextupoles
+%   
+%   see also: findOct
+
+
+indsext=findcells(ring,'Class','Sextupole')';
 b3=getcellstruct(ring,'PolynomB',indsext,1,3);
 L=getcellstruct(ring,'Length',indsext);
 end
